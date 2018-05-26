37c8c046646c57698bb7b89256dc6e28a580e107
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-26 18:41:43 +0200

    finish task

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-var EventEmitter = require("events").EventEmitter,
-    OSinfo = require('./modules/OSinfo'),
-    emitter = new EventEmitter();
-
-emitter.on("beforeCommand", function (instruction) {
-    console.log('You wrote: ' + instruction + ', trying to run command');
-});
-emitter.on("afterCommand", function () {
-    console.log('Finished command');
-});
-
+var os = require('os');
+    
+// SET ENCODING UTF-8
 process.stdin.setEncoding('utf-8');
+
 
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    
-    if(input !== null) {
+    if(input !==null) {
         var instruction = input.trim();
-        emitter.emit('beforeCommand', instruction);
         switch(instruction) {
             case '/exit':
-                process.stdout.write('Quiting app!');
+                process.stdout.write('Quitting app\n');
                 process.exit();
                 break;
             case '/sayhello':
                 process.stdout.write('hello!\n');
                 break;
             case '/getOSinfo':
-                OSinfo.get();
+                var OSinfo = require('/js/modules/OSinfo');
+                OSinfo.print();
                 break;
             default:
                 process.stderr.write('Wrong instruction!\n');
         };
-        emitter.emit('afterCommand');
     }
+    console.log(process.versions); 
 });
