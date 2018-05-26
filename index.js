975591560f8362472adcee4f25701e629f0f872f
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-26 18:49:00 +0200

    correct index.js

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-var os = require('os');
+var os = require('os'),
+    colors = require('colors');
+
+function getOSinfo() {
+    var type = os.type();
     
-// SET ENCODING UTF-8
-process.stdin.setEncoding('utf-8');
-
-
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-    if(input !==null) {
-        var instruction = input.trim();
-        switch(instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app\n');
-                process.exit();
-                break;
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-                break;
-            case '/getOSinfo':
-                var OSinfo = require('/js/modules/OSinfo');
-                OSinfo.print();
-                break;
-            default:
-                process.stderr.write('Wrong instruction!\n');
-        };
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
     }
-    console.log(process.versions); 
-});
+    
+    var release = os.release(),
+        cpu = os.cpus()[0].model,
+        uptime = os.uptime(),
+        userInfo = os.userInfo();
+    
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
+
+//EXPORT getOSinfo
+exports.print = getOSinfo;
